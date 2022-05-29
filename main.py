d9fd26e94c8ba389a37342c6300a464985b81517
100ballovby <legorobotsbel@gmail.com>
2022-05-29 13:12:26 +0300

    Finding elements

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ url = 'https://7745.by/catalog/dreli'
 response = r.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 # Парсить(что, в_формате)
-card = soup.find('div', class_='styles_root__ti07r')  # какой элемент искать, метка для поиска
-print(card)
+card = soup.find('div', class_='catalog-item')  # какой элемент искать, метка для поиска
+art = card.find('div', class_='articul')
+title = card.find('a', class_='item-block_name')
+print(art.text)
+print(title.text, 'https://7745.by' + title.get('href'))
